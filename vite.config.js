2da9c714b364c4c5d9407bb7e336eeb30dfd4c7f
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2025-02-25 02:45:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // Ensure Vite knows this is your static assets folder
   build: {
     outDir: 'dist',
     rollupOptions: {
