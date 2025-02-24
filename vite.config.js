0c03183e6ec81c362708ca9c28af5113ed330a59
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2025-02-25 02:20:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',             // Tells Vite the root is the current folder
-  publicDir: 'public',   // Points to the public folder
   build: {
-    outDir: 'dist'       // Output directory Vercel looks for
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
   }
 });
